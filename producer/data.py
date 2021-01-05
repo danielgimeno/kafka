863f097969b093472200021d3ba26aba542b93dc
Daniel Gimeno <daniel.gimeno@gmail.com>
2021-01-05 22:03:06 +0000

    productor and consumer added

---
+++ producer/data.py
@@ -0,0 +1,12 @@
+from faker import Faker
+
+fake = Faker()
+
+def get_registered_user():
+    return {
+        "name": fake.name(),
+        "address": fake.address(),
+        "created_at": fake.year()
+    }
+
+print (get_registered_user())
